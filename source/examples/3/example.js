51258346b66ed33a46af919371c018b6bb3064f7
scann <apple.eater01@gmail.com>
2019-02-12 17:55:52 +0200

    Order of executing generators

--- source/examples/3/example.js
+++ source/examples/3/example.js
@@ -12,8 +12,13 @@ import { swapiActions } from '../../bus/swapi/actions';
 import { api } from '../../API';
 
 function* fetchVehicles(action) {
+    console.log('-> 2');
     const response = yield call(api.fetchVehicles, action.payload);
+
+    console.log('-> 3');
     const data = yield apply(response, response.json);
+
+    console.log('-> 4');
 
     return data;
 }
@@ -22,8 +27,10 @@ export function* runExample() {
     while (true) {
         const action = yield take(types.FETCH_VEHICLES_ASYNC);
 
+        console.log('-> 1');
         const data = yield* fetchVehicles(action);
 
+        console.log('-> 5');
         yield put(swapiActions.fillVehicles(data.results));
     }
 }
--- source/examples/4/example.js
+++ source/examples/4/example.js
@@ -2,7 +2,7 @@
  * Существуют блокирующие и неблокирующие эффекты.
  * Например, take — блокирующий эффект.
  * Достигнув эффекта take генератор заморозится до тех пор,
- * пока не произойдет dispatch экешна с ожидаемым паттерном.
+ * пока не произойдет dispatch экшена с ожидаемым паттерном.
  *
  * call тоже блокирует поток выполнения генератора в том случае, если ему
  * возвращается промис.
@@ -17,8 +17,13 @@ import { swapiActions } from '../../bus/swapi/actions';
 import { api } from '../../API';
 
 function* fetchVehicles(action) {
+    console.log('-> 2');
     const response = yield call(api.fetchVehicles, action.payload);
+
+    console.log('-> 3');
     const data = yield apply(response, response.json);
+
+    console.log('-> 4');
 
     return data;
 }
@@ -26,8 +31,10 @@ function* fetchVehicles(action) {
 export function* runExample() {
     while (true) {
         const action = yield take(types.FETCH_VEHICLES_ASYNC);
+        console.log('-> 1');
 
         const data = yield call(fetchVehicles, action);
+        console.log('-> 5');
 
         yield put(swapiActions.fillVehicles(data.results));
     }
